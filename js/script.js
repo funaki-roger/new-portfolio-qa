3908899fb6a5cb52c50d8e0439e645fa191bf487
funaki-roger <rogerfunaki@outlook.com>
2024-09-12 16:05:33 -0300

    add: text typing animation on home page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    strings: ["", "I work with", "WEB | Mobile | API | Scrum Master"],
+    typeSpeed: 150,
+    backSpeed: 250,
+    loop: true
+});
